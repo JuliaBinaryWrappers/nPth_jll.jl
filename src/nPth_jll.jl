181122a968830bacde92b637001ae626ee472f3d
jlbuild <juliabuildbot@gmail.com>
2025-12-28 17:36:28 +0000

    nPth_jll build 1.8.1+0

--- src/nPth_jll.jl
+++ src/nPth_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("nPth")
-JLLWrappers.@generate_main_file("nPth", UUID("58695248-dc95-54d8-8aad-a18bc9fe19aa"))
+JLLWrappers.@generate_main_file("nPth", Base.UUID("58695248-dc95-54d8-8aad-a18bc9fe19aa"))
 end  # module nPth_jll
